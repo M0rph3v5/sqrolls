3f987aa3e68ad30ffcebb4564221721855bf928d
Fedor <team@orbitgametech.com>
2013-04-27 12:32:55 +0200

    initial render

--- src/components/Tile.hx
+++ src/components/Tile.hx
@@ -3,7 +3,6 @@ package components;
 using Imports;
 
 class Tile {
-	public var onChanged:Signal1<Tile>;
 	public var onTileItemAdded:Signal2<Tile, TileItem>; 
 	
 	public var pos:Vec2;
@@ -12,13 +11,11 @@ class Tile {
 	public function new(pos:Vec2){
 		this.pos = pos;
 		stack = new Array<TileItem>();
-		onChanged = new Signal1();
 		onTileItemAdded = new Signal2();
 	}
 	
 	public function push(tileItem:TileItem){
 		stack.push(tileItem);
-		onChanged.dispatch(this);
 		onTileItemAdded.dispatch(this, tileItem);
 	}
 }
--- src/systems/GridRenderS.hx
+++ src/systems/GridRenderS.hx
@@ -17,16 +17,20 @@ class GridRenderS extends ListIteratingSystem<GridRenderN>{
 	
 	function add(node:GridRenderN){
 		node.grid.tiles.walk(function(current:Tile,x,y){
-			current.onChanged.add(onTileChanged);
+			for(tileItem in current.stack){
+				onTileItemAdded(current, tileItem);
+			}
+			current.onTileItemAdded.add(onTileItemAdded);
 			return current;
 		});
 	}
 	
-	function onTileChanged(tile:Tile){
-		/*var textField:TextField = new TextField(50,50,"9");
-		textField.x = 100;
-		textField.color = 0xffffff;
-		container.addChild(textField);*/		
+	function onTileItemAdded(tile:Tile, tileItem:TileItem){
+		tileItem.textField = new TextField(50, 50, tileItem.number + "");
+		tileItem.textField.x = tile.pos.x * 50;
+		tileItem.textField.y = tile.pos.y * 50;
+		tileItem.textField.color = 0xffffff;
+		container.addChild(tileItem.textField);
 	}
 	
 	function remove(node:GridRenderN){
