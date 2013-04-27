a7b571c6d2e26d6fa99c9cc17b9bf013da1956f1
Benjamin de Jager <m0rph3v5@gmail.com>
2013-04-27 13:07:02 +0200

    grid has size getters

--- src/components/Grid.hx
+++ src/components/Grid.hx
@@ -6,8 +6,12 @@ class Grid {
 	public var tiles:Array2<Tile>;
 	public var total:Int;
 	
+	public var columns:Int = 6;	 
+	public var rows:Int = 6;
+	public var tileSize:Int = 50;
+	
 	public function new(){
-		tiles = new Array2(6,6);
+		tiles = new Array2(columns,rows);
 		tiles.walk(function(current,x,y){
 			return new Tile(new Vec2(x,y));
 		});
@@ -16,5 +20,13 @@ class Grid {
 			current.push(new TileItem(Random.randRange(0,9)));
 			return current;
 		});
+	}
+	
+	public function width():Int {
+		return tileSize * columns;
+	}
+	
+	public function height():Int {
+		return tileSize * rows;
 	}
 }
